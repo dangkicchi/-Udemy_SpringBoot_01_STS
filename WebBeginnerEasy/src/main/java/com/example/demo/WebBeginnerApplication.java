334d918e35d44b6eb297fb702484ebb12f456d82
dangkicchi <toshiro.fujisaki@gmail.com>
2021-06-03 22:47:59 +0900

    gradle まわりを修正

--- WebBeginnerEasy/src/main/java/com/example/demo/WebBeginnerApplication.java
+++ WebBeginnerEasy/src/main/java/com/example/demo/WebBeginnerApplication.java
@@ -2,9 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebBeginnerApplication {
+public class WebBeginnerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebBeginnerApplication.class, args);
